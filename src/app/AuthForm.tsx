a28a6f23e9fbd7eac011f528e127288708d9b1ee
jfinfocesde <110859321+jfinfocesde@users.noreply.github.com>
2023-12-20 15:33:09 -0500

    20-12-2023

--- src/app/AuthForm.tsx
+++ src/app/AuthForm.tsx
@@ -16,7 +16,8 @@ export default function AuthForm() {
       showLinks={false}
       onlyThirdPartyProviders={true}
       providers={['github','google']}
-      redirectTo="http://localhost:3000/api/auth/callback"
+      redirectTo="https://jcode-khaki.vercel.app/api/auth/callback"
+      // redirectTo="http://localhost:3000/api/auth/callback"
     />
   )
 }
