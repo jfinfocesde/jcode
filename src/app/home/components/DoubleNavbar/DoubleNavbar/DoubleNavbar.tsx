fc5d40cd2ee359156210f085bb357749e88093d7
jfinfocesde <110859321+jfinfocesde@users.noreply.github.com>
2023-12-24 15:00:10 -0500

    24-12-2023

--- src/app/home/components/DoubleNavbar/DoubleNavbar/DoubleNavbar.tsx
+++ src/app/home/components/DoubleNavbar/DoubleNavbar/DoubleNavbar.tsx
@@ -47,7 +47,7 @@ export function DoubleNavbar({ onToggle }: { onToggle: () => void }) {
       <UnstyledButton
         onClick={() => {
           try {
-            // onToggle()
+            onToggle()
             dispatch(reduxChangeSidebar(link.sisdebar))
             switch (link.label) {
               case 'Incio':
@@ -82,7 +82,7 @@ export function DoubleNavbar({ onToggle }: { onToggle: () => void }) {
       href={'#'}
       onClick={async (event) => {
         event.preventDefault();
-        // onToggle()
+        onToggle()
         dispatch(reduxUpdateSelectItem(index))
         setActiveLink(link);
       }}
