c3b3705ac6a896fa9f62b8a1a5eb0b4036a416d1
jfinfocesde <110859321+jfinfocesde@users.noreply.github.com>
2023-12-25 19:31:32 -0500

    25-12-2023

--- next.config.js
+++ next.config.js
@@ -1,78 +1,79 @@
-// // /** @type {import('next').NextConfig} */
-// // const nextConfig = {}
-
-// // module.exports = nextConfig
-
-
-// // const withMDX = require('@next/mdx')()
-
-// const {
-//     remarkCodeHike,
-// } = require("@code-hike/mdx")
-
-// const withMDX = require("@next/mdx")({
-//     extension: /\.mdx?$/,
-//     options: {
-//         remarkPlugins: [
-//             [remarkCodeHike, { theme: "dracula-soft", showCopyButton: true }]
-//         ],
-//     },
-// })
-
-
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {
+// const nextConfig = {}
 
-//     transpilePackages: ['@mdxeditor/editor', 'react-diff-view'],
-//     reactStrictMode: true,
-//     webpack: (config) => {
-//         // this will override the experiments
-//         config.experiments = { ...config.experiments, topLevelAwait: true };
-//         // this will just update topLevelAwait property of config.experiments
-//         // config.experiments.topLevelAwait = true 
-//         return config;
-//     },
-//     // Configure `pageExtensions` to include MDX files
-//     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-//     // Optionally, add any other Next.js config below
-// }
+// module.exports = nextConfig
 
-// module.exports = withMDX(nextConfig)
+
+// const withMDX = require('@next/mdx')()
 
 const {
     remarkCodeHike,
 } = require("@code-hike/mdx")
 
-const withPlugins = require('next-compose-plugins');
-const withMDX = require('@next/mdx')({
+const withMDX = require("@next/mdx")({
     extension: /\.mdx?$/,
     options: {
         remarkPlugins: [
             [remarkCodeHike, { theme: "dracula-soft", showCopyButton: true }]
         ],
     },
-});
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    skipWaiting: true,
-});
+})
 
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+
     transpilePackages: ['@mdxeditor/editor', 'react-diff-view'],
     reactStrictMode: true,
     webpack: (config) => {
+        // this will override the experiments
         config.experiments = { ...config.experiments, topLevelAwait: true };
+        // this will just update topLevelAwait property of config.experiments
+        // config.experiments.topLevelAwait = true 
         return config;
     },
+    // Configure `pageExtensions` to include MDX files
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    // Optionally, add any other Next.js config below
 }
 
-module.exports = withPlugins([
-    withMDX,
-    withPWA
-], nextConfig);
+module.exports = withMDX(nextConfig)
+
+// const {
+//     remarkCodeHike,
+// } = require("@code-hike/mdx")
+
+// const withPlugins = require('next-compose-plugins');
+// const withMDX = require('@next/mdx')({
+//     extension: /\.mdx?$/,
+//     options: {
+//         remarkPlugins: [
+//             [remarkCodeHike, { theme: "dracula-soft", showCopyButton: true }]
+//         ],
+//     },
+// });
+
+// const withPWA = require('next-pwa')({
+//     dest: 'public',
+//     disable: process.env.NODE_ENV === 'development',
+//     register: true,
+//     skipWaiting: true,
+// });
+
+// const nextConfig = {
+//     transpilePackages: ['@mdxeditor/editor', 'react-diff-view'],
+//     reactStrictMode: true,
+//     webpack: (config) => {
+//         config.experiments = { ...config.experiments, topLevelAwait: true };
+//         return config;
+//     },
+//     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+// }
+
+// module.exports = withPlugins([
+//     withMDX,
+//     withPWA
+// ], nextConfig);
 
 
 
