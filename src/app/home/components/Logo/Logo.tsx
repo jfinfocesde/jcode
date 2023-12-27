9237e2299376935947a0d0824130c4d3840f7e1e
jfinfocesde <110859321+jfinfocesde@users.noreply.github.com>
2023-12-27 11:28:31 -0500

    27-12-2023

--- src/app/home/components/Logo/Logo.tsx
+++ src/app/home/components/Logo/Logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Logo = () => {
   return (
     <div className="logo-container" style={{ display: 'flex', alignItems: 'center' }}>
-      <Image src="/logo7.png" alt="me"  width={120}  height={36}/>      
+      <Image src="/logo8.png" alt="me"  width={120}  height={36}/>      
     </div>
   );
 };
